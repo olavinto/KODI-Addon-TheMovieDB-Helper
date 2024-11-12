353103ff0f0cbf8b696cc00f92736e0959935520
jurialmunkey <jurialmunkey@kodi.tv>
2024-11-12 12:41:44 +1100

    :zap: Avoid Kodi RuntimeError exception exiting imgmon thread

--- resources/tmdbhelper/lib/monitor/imgmon.py
+++ resources/tmdbhelper/lib/monitor/imgmon.py
@@ -1,7 +1,7 @@
 from tmdbhelper.lib.monitor.images import ImageManipulations
 from tmdbhelper.lib.monitor.poller import Poller, POLL_MIN_INCREMENT
 from tmdbhelper.lib.monitor.listitemtools import ListItemInfoGetter
-# from tmdbhelper.lib.addon.plugin import get_setting
+from tmdbhelper.lib.addon.logger import kodi_try_except
 from threading import Thread
 
 
@@ -18,11 +18,15 @@ class ImagesMonitor(Thread, ListItemInfoGetter, ImageManipulations, Poller):
     def setup_current_container(self):
         self._container_item = self.container_item
 
-    def _on_listitem(self):
+    @kodi_try_except('lib.monitor.imgmon.on_listitem')
+    def on_listitem(self):
         self.setup_current_container()
         if self.pre_item != self.cur_item:
             self.get_image_manipulations(use_winprops=True)
             self.pre_item = self.cur_item
+
+    def _on_listitem(self):
+        self.on_listitem()
         self._on_idle(POLL_MIN_INCREMENT)
 
     def _on_scroll(self):
